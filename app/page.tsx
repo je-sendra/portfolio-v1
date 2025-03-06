5b4f672cb9a3c8e6830c584b08db7d46f053e254
jsendra <jsendra@mafroda.es>
2025-03-06 14:09:32 +0100

    feat: remove Contact component from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Header from "./components/Header";
 import MainSection from "./components/MainSection";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 export default function Home() {
@@ -17,7 +16,6 @@ export default function Home() {
         <AboutMe />
         <Skills />
         <Projects />
-        <Contact />
       </main >
       <Footer />
     </>
