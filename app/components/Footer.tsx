7e6730a4f02b731d6dac0602840397b570ec3574
jsendra <jsendra@mafroda.es>
2025-03-06 14:11:17 +0100

    feat: update Footer component to reflect Josep Sendra's information and remove Twitter link

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,4 @@
 import { AiFillCode } from "react-icons/ai";
-import { BsTwitterX } from "react-icons/bs";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa6";
 
@@ -9,21 +8,16 @@ const Footer = () => {
         <footer className="w-full bg-slate-900/30 border-solid border-t-2 border-yellow-300">
             <div className="w-10/12 2xl:w-7/12 mx-auto py-10 flex items-center justify-between flex-col md:flex-row gap-4 md:gap-0">
                 <p className="flex items-center gap-2">
-                    <span className="text-xl text-yellow-300"><AiFillCode /></span> Diego Barrera / 2024
+                    <span className="text-xl text-yellow-300"><AiFillCode /></span> Josep Sendra / 2025
                 </p>
                 <div className="flex items-center justify-center gap-3">
-                    <a href="https://twitter.com/turingdb" target="_blank" rel="noopener noreferrer" className="text-xl hover:text-yellow-300"
-                        aria-label="Go to Diego's Twitter(x) profile"
-                    >
-                        <BsTwitterX />
-                    </a>
-                    <a href="https://www.linkedin.com/in/diegobarreracoba/" target="_blank" rel="noopener noreferrer" className="text-xl hover:text-yellow-300"
-                          aria-label="Go to Diego's Linkedin profile"
+                    <a href="https://www.linkedin.com/in/josep-sendra-serra/" target="_blank" rel="noopener noreferrer" className="text-xl hover:text-yellow-300"
+                          aria-label="Go to Josep's Linkedin profile"
                     >
                         <FaLinkedin />
                     </a>
-                    <a href="https://github.com/Diego-Barrera-07" target="_blank" rel="noopener noreferrer" className="text-xl hover:text-yellow-300"
-                          aria-label="Go to Diego's Github profile"
+                    <a href="https://github.com/je-sendra" target="_blank" rel="noopener noreferrer" className="text-xl hover:text-yellow-300"
+                          aria-label="Go to Josep's Github profile"
                     >
                         <FaGithub />
                     </a>
